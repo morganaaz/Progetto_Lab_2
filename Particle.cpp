60fb07d62e2bfe73d4b96d032b5a7105c43e472d
Morgana Zulli <134314057+morganaaz@users.noreply.github.com>
2023-11-21 12:00:51 +0100

    Update Particle.cpp

--- Particle.cpp
+++ Particle.cpp
@@ -1,73 +1,60 @@
+#ifndef PARTICLE_HPP
+#define PARTICLE_HPP
+
 #include "ParticleType.hpp"
 #include "ResonanceType.hpp"
-#include "Particle.hpp"
+#include <array>
+#include <cmath> //
 
-#include <iostream>
-//includo <cstring> per la funzione strcmp
-#include <cstring>
+class Particle
+{
+public:
+    Particle(const char *name, double const fPx, double const fPy, double const fPz);
+    Particle();//
 
-int Particle::fNParticleType=0;
+    //static void ArrayPrint();
+    void ParticlePrint();
 
-//essendo static è necessaria una def out of line
-std::array<ParticleType*, Particle::fMaxNumParticleType> Particle::fParticleType;
-//il tipo va specificato quando non è un costruttore
-int Particle::FindParticle(const char* name){
-                int index=13;
-                static int fNParticleType=0;
-                //while(i<10){
-                while(fNParticleType<fMaxNumParticleType){
-                    //if(name==fParticleType[i]->getName()){
-                    //== compara gli indirizzi di memoria delle variabili, non il contenuto
-                    if(strcmp(name, fParticleType[fNParticleType]-> getName())==0){
-                        index=fNParticleType;
-                        break;
-                    }
-                    ++fNParticleType;
-                }
-                //return(index);
-                if (index==13){
-                    std::cout<<"No corresponding type found"<<'\n';
-                }
-                return index;
-};
+    double getFPx();
+    double getFPy();
+    double getFPz();
+    int getfIndex() const;
 
-
-//APT prende l'output di FP e se risulta una nuova particella,
-//l'aggiunge all'array
-//non prende nulla in input
-//se FP(name)==13 
-void Particle::AddParticleType(int fIndex){
-    if(fNParticleType<fMaxNumParticleType){
-        const char* newName = fParticleType[fIndex]->getName();
-        if (FindParticle(newName)==13){
-            fParticleType[fNParticleType++]= new ParticleType(
-                fParticleType[fIndex]->getName(),
-                fParticleType[fIndex]->getMass(),
-                fParticleType[fIndex]->getCharge()
-            );
-            std::cout<<"New particle added"<<'\n';
-        }
-        else {
-            std::cout<<"Particle type already exists"<<'\n';
-        }
-    }
-    else {
-        std::cout<<"Max number of particles reached"<<'\n';
-    }
-}
-
-Particle::Particle(const char* name, double const fPx, double const fPy, double const fPz):
-                   fPx_(fPx), fPy_(fPy), fPz_(fPz), fIndex(FindParticle(name)) {}
-
-
-static void ArrayPrint(){
+    double GetMass() const;
+    double GetEnergy(double mass, double fpx, double fpy, double fpz) const;
+    double InvMass(const Particle particle, double particleEnergy, const Particle p, double pEnergy) const;
     
+    void SetP(double px, double py, double pz);
+    void SetfIndex(int index_);
+    void SetfIndex(const char *name);
+    //void PrintArray() const;
+    //void PrintParticle() const;
+
+    //static void AddParticleType(int);
+    static void AddParticle(const char* name, double mass,
+                            int charge, double width = 0);
+
+    int Decay2body(Particle &dau1, Particle &dau2) const;
+    void Boost(double bx, double by, double bz);
+
+    static void PrintArray();
+    void PrintParticle(); 
+
+    //void setFPx(double px);
+    //void setFPy(double py);
+    //void setFPz(double pz);
+
+private:
+    static const int fMaxNumParticleType = 10;
+    static std::array<ParticleType *, fMaxNumParticleType> fParticleType;
+    // definisco un array statico con fMNPT elementi di tipo PT, lo chiamo fPT
+    static int fNParticleType; // ciclo while in cpp
+    int fIndexParticle = -1; //
+    int fIndex;
+    static int FindParticle(const char *name);
+
+    double FPx_ = 0;
+    double FPy_ = 0;
+    double FPz_ = 0;
 };
-void ParticlePrint();
-const double getFPx();
-const double getFPy();
-const double getFPz();
-const double Mass();
-const double Energy();
-double InvMass(const ParticleType Particle);
-void SetP(double px, double py, double pz);
+#endif
